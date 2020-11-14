404c22c255dc5815ccf648768b9b4a6ee9b679e3
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-11-14 17:02:18 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -14,9 +14,14 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
+        
+        
      }
-
-
+     return programmer_hash[:grace_hopper]
+   
+    
+       
+     
 end
 
 def alan_kay_is_known_for
@@ -36,7 +41,9 @@ def alan_kay_is_known_for
           :known_for => "Unix",
           :languages => ["C"]
         }
+        
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +62,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+       programmer_hash[:dennis_ritchie][:languages][0]
+       
+       
 end
 
 def adding_matz
@@ -64,8 +74,11 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
- 		{
+  programmer_hash = 
+  		{ :yukihiro_matsumoto =>{
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      },        
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -77,10 +90,13 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+      
         }
-     }
-
-    
+        
+        
+     }    
+     programmer_hash
+     
 end
 
 def changing_alan
@@ -100,26 +116,28 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     alans_new_info = programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
      
 end
 
 def adding_to_dennis
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
+          :languages => ["Smalltalk", "LISP"]},
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+        
+      }
+      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+      programmer_hash
+      
 end
